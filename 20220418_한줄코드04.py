de1195b8357b0dbf35ac280511e175afd4188386
NeovistaDino <103423410+NeovistaDino@users.noreply.github.com>
2022-04-18 20:48:21 +0900

    Create 20220418_한줄코드04.py
    ""를 쓸떄 조심하자

---
+++ 20220418_한줄코드04.py
@@ -0,0 +1,14 @@
+print("%d" "%d" %(20, 29))
+
+# TypeError: not all arguments converted  during string formatting
+"""
+Arguments : 인수, 인자
+converted : 전환하다, 전환된
+during : ~동안, ~중에
+string : 줄, 문자열
+formatting : 치환
+
+해결:
+print("%d, %d" %(20,29))
+"""
+

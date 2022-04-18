0431f72c04007ed2d3044591cbb0de9089648dbf
NeovistaDino <103423410+NeovistaDino@users.noreply.github.com>
2022-04-18 22:19:43 +0900

    Create 20220418_세줄코드02.py
    오류 해결

---
+++ 20220418_세줄코드02.py
@@ -0,0 +1,18 @@
+dino_age = "20"
+#dino_age에 string"20"저장
+
+young_age = "29"
+#young_age에 string"29"저장
+
+print("%s, %s" %(dino_age, young_age))
+#출력 format %s는 2개 string은 2개(dino_age, young_age)
+
+"""
+string변수 : 2개(dino_age, young_age)가 "20","29"의 값을 입력 받고
+출력 "%s, %s" 각각 dino_age, young_age의 문자형"20", "29"가 출력된다
+
+
+함수 print() 이용해서 출력하시오.
+
+결과:
+20, 29

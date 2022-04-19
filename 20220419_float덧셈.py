90a1d826836a70d869f1ecbbac295b5c6a8cd13e
NeovistaDino <103423410+NeovistaDino@users.noreply.github.com>
2022-04-19 19:41:34 +0900

    Create 20220419_float덧셈.py
    ("%.2f")해야함

---
+++ 20220419_float덧셈.py
@@ -0,0 +1,28 @@
+float_num1 = 1.5
+float_num2 = 3.27
+
+# float_num1+float_num2 = sum
+# SyntaxError: cannot assign to operator
+
+float_sum = float_num1 + float_num2
+
+print("%.2f" %(float_sum))
+
+
+
+
+
+
+
+"""
+float_num1 = 1.5
+
+float_num2 = 3.27
+
+float_sum = float_num1 + float_num2
+
+print(float_sum)
+
+결과:
+4.77
+"""
